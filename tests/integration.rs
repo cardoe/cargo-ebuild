1484cdbaeada082a01027c0c72b94af36d6fe071
gibix <gibix@riseup.net>
2018-05-15 15:35:50 +0200

    add basic test
    Signed-off-by: gibix <gibix@riseup.net>

---
+++ tests/integration.rs
@@ -0,0 +1,24 @@
+extern crate assert_cli;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+// TODO: inspect use of tempdir/tempfile
+//
+#[test]
+fn ebuild() {
+    assert_cli::Assert::main_binary()
+        .stdout().is("Wrote: cargo-ebuild-0.1.7.ebuild")
+        .unwrap();
+
+    let mut new_file = File::open("cargo-ebuild-0.1.7.ebuild").unwrap();
+    let mut new_ebuild = String::new();
+    new_file.read_to_string(&mut new_ebuild).unwrap();
+
+    let mut test_file = File::open("tests/cargo-ebuild-0.1.7.ebuild").unwrap();
+    let mut test_ebuild = String::new();
+    test_file.read_to_string(&mut test_ebuild).unwrap();
+
+
+    assert_eq!(new_ebuild, test_ebuild);
+}

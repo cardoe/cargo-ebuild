b4388d095d2d6b3fbff13f27de04fc0926453314
gibix <gibix@riseup.net>
2018-05-15 17:09:17 +0200

    add travis cache
    Signed-off-by: gibix <gibix@riseup.net>

--- tests/integration.rs
+++ tests/integration.rs
@@ -12,15 +12,19 @@ fn ebuild() {
         .stdout().is("Wrote: cargo-ebuild-0.1.7.ebuild")
         .unwrap();
 
-    let mut new_file = File::open("cargo-ebuild-0.1.7.ebuild").unwrap.or_else(|| {
-        panic!("couldn't open generated ebuild: {}", why.description());
-    });
+    let mut new_file = match File::open("cargo-ebuild-0.1.7.ebuild") {
+        Err(why) => panic!("couldn't open generated ebuild: {}", why),
+        Ok(f) => f,
+    };
+
     let mut new_ebuild = String::new();
     new_file.read_to_string(&mut new_ebuild).unwrap();
 
-    let mut test_file = File::open("tests/cargo-ebuild-0.1.7.ebuild").unwrap.or_else(|| {
-        panic!("couldn't open test ebuild: {}", why.description());
-    });
+    let mut test_file = match File::open("tests/cargo-ebuild-0.1.7.ebuild") {
+        Err(why) => panic!("couldn't open generated ebuild: {}", why),
+        Ok(f) => f,
+    };
+
     let mut test_ebuild = String::new();
     test_file.read_to_string(&mut test_ebuild).unwrap();
 
